97b37a65be8869c14d40b1501fea301b4e2ad9b8
Matthias Ludwig <mludwig@quobject.io>
2021-03-03 11:41:10 +1100

    2.2.1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -39,30 +39,30 @@ describe('iam list-users', () => {
 });
 
 
-// describe('iam list-users', () => {
-//   it('should fail with invalid sessionToken', () => {
-//     const result = true;
+describe('iam list-users', () => {
+  it('should fail with invalid sessionToken', () => {
+    const result = true;
 
-//     expect(result).toBeTruthy();
-//     const options = new Options(
-//       /* accessKey    */ config.accessKeyId,
-//       /* secretKey    */ config.secretAccessKey,
-//       /* sessionToken */ 'invalid',
-//       /* currentWorkingDirectory */ undefined,
-//     );
-
-
-//     const aws = new Aws(options);
-
-//     let flag = false;
-//     return aws.command('iam list-users').then((data: any) => {
-//       flag = true;
-//     }).catch((r) => {
-//       expect(flag).toBeFalsy();
-//       //console.log('r = ', r);
-//     });
+    expect(result).toBeTruthy();
+    const options = new Options(
+      /* accessKey    */ config.accessKeyId,
+      /* secretKey    */ config.secretAccessKey,
+      /* sessionToken */ 'invalid',
+      /* currentWorkingDirectory */ undefined,
+    );
 
 
-//   });
-// });
+    const aws = new Aws(options);
+
+    let flag = false;
+    return aws.command('iam list-users').then((data: any) => {
+      flag = true;
+    }).catch((r) => {
+      expect(flag).toBeFalsy();
+      //console.log('r = ', r);
+    });
+
+
+  });
+});
 
